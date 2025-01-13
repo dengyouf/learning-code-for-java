74d7be14af1de527f6865414497621f8537c8a6b
dengyouf <1071102039@qq.com>
2025-01-13 08:39:07 +0800

    add java

---
+++ build/_sources/index.rst.txt
@@ -0,0 +1,10 @@
+.. Learning Code for Java documentation master file, created by
+   sphinx-quickstart on Mon Jan 13 08:29:09 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. toctree::
+   :maxdepth: 3
+   :caption:  编程语言
+
+   java/java_basic.md
---
+++ source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Learning Code for Java'
+copyright = '2025, Deng You'
+author = 'Deng You'
+release = 'v0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['recommonmark','sphinx_markdown_tables']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinxdoc"
+
+html_theme_options = {
+    "rightsidebar": "true",
+    "relbarbgcolor": "black"
+}
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    "navigation_depth": 4,  # 或更高，控制左侧栏的深度
+}
+html_static_path = ['_static']
+
+html_search_language = 'zh_CN'
